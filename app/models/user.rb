9cbf411b79012c6da80f971b8d9cdd73b55cd9c8
samaimo <kii328815@gmail.com>
2024-03-14 08:53:43 +0900

    viewsの置き換え

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  # 入力必須（ニックネーム、氏名、カナ氏名、誕生日）
+  validates :nickname, :last_name, :first_name, :kana_first_name, :kana_last_name, :birth_day,
+            presence: true
+
+  # お名前（全角）は全角（漢字/ひらがな/カタカナ）が必須
+  validates :last_name, :first_name, presence: true,
+                                     format: { with: /\p{Han}|\p{Katakana}|\p{Hiragana}|[ー〜々〆〤]+/, message: '全角の漢字・ひらがな・カタカナで入力してください' }
+  # お名前カナ(全角)は、全角（カタカナ）での入力が必須
+  validates :kana_last_name, :kana_first_name, presence: true,
+                                               format: { with: /\A[ァ-ヶー]+\z/, message: '全角カタカナで入力してください' }
+
+  private
+end
