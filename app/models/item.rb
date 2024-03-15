7c7dae07c12fbebb4c7f1d62f5d117cf03ca8cfa
samaimo <kii328815@gmail.com>
2024-03-15 12:07:38 +0900

    配送料負担区分のプルダウン実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,8 +4,10 @@ class Item < ApplicationRecord
             :shipping_cost_burden_id, :delivery_days_id, :prefecture_id,
             :price, :user,
             presence: true
-  # 商品カテゴリーの選択が「---」の時は保存できないようにする
+  # プルダウン式の選択が「---」の時は保存できないようにする
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_cost_burden_id_id, numericality: { other_than: 1, message: "can't be blank" }
 
   # アソシエーション
   belongs_to :user
@@ -13,4 +15,5 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
+  belongs_to :shipping_cost_burden
 end
