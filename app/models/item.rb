0443c76d1483b110395dfff1064eb43b2a3e047d
samaimo <kii328815@gmail.com>
2024-03-15 11:05:24 +0900

    商品カテゴリー選択時のバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,12 @@ class Item < ApplicationRecord
             :shipping_cost_burden_id, :delivery_days_id, :prefecture_id,
             :price, :user,
             presence: true
+  # 商品カテゴリーの選択が「---」の時は保存できないようにする
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+
   # アソシエーション
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
 end
