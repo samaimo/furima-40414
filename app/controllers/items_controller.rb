6b81b22432840ce33110c17e661a86ca1f46e5b3
samaimo <kii328815@gmail.com>
2024-03-16 15:30:11 +0900

    コントロール設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   def index
-    @items = Items.all
+    @items = Item.all
   end
 
   def new
@@ -8,5 +8,18 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :description, :category_id, :condition_id, :shipping_cost_burden_id,
+                                 :delivery_days_id, :prefecture_id, :price, :image).merge(user_id: current_user.id)
   end
 end
