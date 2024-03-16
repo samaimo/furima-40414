66512446ced596473ba4df024488b361fb9934a7
samaimo <kii328815@gmail.com>
2024-03-16 09:18:03 +0900

    アクティブハッシュで追加したカラム名の表記を修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,16 +8,17 @@ class Item < ApplicationRecord
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :shipping_cost_burden_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :prefacture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :delivery_days_id, numericality: { other_than: 1, message: "can't be blank" }
 
   # アソシエーション
   belongs_to :user
+  has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
   belongs_to :shipping_cost_burden
-  belongs_to :prefacture
+  belongs_to :prefecture
   belongs_to :delivery_days
 end
