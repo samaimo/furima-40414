3e627de599b2f6c8d094fd794f64b2356631a468
samaimo <kii328815@gmail.com>
2024-03-18 00:24:47 +0900

    単体テストコードとバリデーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,19 @@
 class Item < ApplicationRecord
   # 入力必須項目
-  validates :item_name, :description, :category_id, :condition_id,
-            :shipping_cost_burden_id, :delivery_days_id, :prefecture_id,
-            :price, :user,
-            presence: true
+  validates :item_name, :description, :category_id, :condition_id, :shipping_cost_burden_id, :delivery_days_id, :prefecture_id, :price,
+            presence: { message: 'を入力してください' }
+
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
+                                    message: 'は300円以上9,999,999円以内の半角数値で入力してください' }
+
+  validates :user, presence: true
+  validates :image, presence: { message: '画像を添付してください' }
   # プルダウン式の選択が「---」の時は保存できないようにする
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :shipping_cost_burden_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :delivery_days_id, numericality: { other_than: 1, message: "can't be blank" }
-
   # アソシエーション
   belongs_to :user
   has_one_attached :image
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,67 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '出品登録' do
+    context '内容に問題ない場合' do
+      it 'すべての情報があれば登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+    context '内容に問題がある場合' do
+      it '商品名が空だと登録できない' do
+        @item.item_name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include 'Item name を入力してください'
+      end
+      it '商品説明が空だと登録できない' do
+        @item.description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include 'Description を入力してください'
+      end
+      it 'カテゴリーが空だと登録できない' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include 'Category を入力してください'
+      end
+      it '商品の状態が空だと登録できない' do
+        @item.condition_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include 'Condition を入力してください'
+      end
+      it '配送料の負担が空だと登録できない' do
+        @item.shipping_cost_burden_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include 'Shipping cost burden を入力してください'
+      end
+      it '発送元が空だと登録できない' do
+        @item.prefecture_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include 'Prefecture を入力してください'
+      end
+      it '発送までの日数が空だと登録できない' do
+        @item.delivery_days_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include 'Delivery days を入力してください'
+      end
+      it '販売価格が空だと登録できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include 'Price は300円以上9,999,999円以内の半角数値で入力してください'
+      end
+      it '販売価格は半角数値のみ登録可能' do
+        @item.price = '１１１１'
+        @item.valid?
+        expect(@item.errors.full_messages).to include 'Price は300円以上9,999,999円以内の半角数値で入力してください'
+      end
+      it '画像が空だと登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include 'Image 画像を添付してください'
+      end
+    end
+  end
 end
