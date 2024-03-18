6bcb965decc69fc2d29d88132840d472dc37db6b
samaimo <kii328815@gmail.com>
2024-03-18 21:41:21 +0900

    テストコード「ユーザーが紐づいていないと保存できない」を実装

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -72,6 +72,11 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include 'Price は300円以上9,999,999円以内の半角数値で入力してください'
       end
+      it 'ユーザーが紐づいていないと保存できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
     end
   end
 end
