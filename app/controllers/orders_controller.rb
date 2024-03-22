e0a797203c56de2398faa5e841f51bf706405ac6
samaimo <kii328815@gmail.com>
2024-03-22 14:51:48 +0900

    Ordersコントローラの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,22 +1,28 @@
 class OrdersController < ApplicationController
 
   def index 
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @item = Item.find(params[:item_id])
-    if user_signed_in? && !Order.exists?(item_id: @item.id) && @item.user == !current_user
-
-      @purchase_form = PurchaseForm.new
-    else
-      redirect_to root_path
+    if user_signed_in? && @item.user != current_user
+      unless Order.exists?(item_id: @item.id, user_id: current_user.id)
+        @purchase_form = PurchaseForm.new
+        return
+      end
     end
+    redirect_to root_path
   end
+  
 
   def create
     @item = Item.find(params[:item_id]) 
     @purchase_form = PurchaseForm.new(purchase_form_params)
+    # 入力フォームに購入情報や配送先情報があり、クレカ情報のトークンがあれば、「購入」できる
     if @purchase_form.valid?
+      pay_item
       @purchase_form.save
       redirect_to root_path
     else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       render :index, status: :unprocessable_entity
     end
   end
@@ -24,7 +30,15 @@ class OrdersController < ApplicationController
   private
 
   def purchase_form_params
-    params.require(:purchase_form).permit(:postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number). merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:purchase_form).permit(:postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token], price: params[:price])
   end
 
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount:  @item.price,
+      card: purchase_form_params[:token],
+      currency: 'jpy'
+    )
+  end
 end
