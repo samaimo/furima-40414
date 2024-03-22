71a16423cbf742d2ee029555e78de9b7a100b1fb
samaimo <kii328815@gmail.com>
2024-03-22 14:51:17 +0900

    販売手数料計算のコードの修正

--- app/javascript/profit.js
+++ app/javascript/profit.js
@@ -3,11 +3,13 @@ window.addEventListener('turbo:load', () => {
   const profitInput = document.getElementById('item-price');
   
   // 入力のたびに処理がされるようにする
+  if (profitInput) {
   profitInput.addEventListener("input", () => {
     const inputValue = profitInput.value;
     const taxPrice = document.getElementById('add-tax-price');
     const taxPriceValue = Math.floor(parseFloat(inputValue) * 0.1) || 0;
     const profitDom = document.getElementById('profit');
-    profitDom.innerHTML = inputValue - taxPriceValue ||0;
-  })
+    profitDom.innerHTML = inputValue - taxPriceValue || 0;
+    });
+  }
 });
