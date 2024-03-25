20d915316e9743870ffb983f413b2ddb25084d1c
samaimo <kii328815@gmail.com>
2024-03-25 20:15:36 +0900

    エラー時の手数料計算表示の実装

--- app/javascript/profit.js
+++ app/javascript/profit.js
@@ -1,15 +1,16 @@
-window.addEventListener('turbo:load', () => {
+function calculateProfit() {
   // 入力された金額を取得し、profitに代入
   const profitInput = document.getElementById('item-price');
   
   // 入力のたびに処理がされるようにする
   if (profitInput) {
   profitInput.addEventListener("input", () => {
-    const inputValue = profitInput.value;
-    const taxPrice = document.getElementById('add-tax-price');
-    const taxPriceValue = Math.floor(parseFloat(inputValue) * 0.1) || 0;
+    const inputValue = parseFloat(profitInput.value);
+    const taxPriceValue = Math.floor(inputValue * 0.1) || 0;
     const profitDom = document.getElementById('profit');
     profitDom.innerHTML = inputValue - taxPriceValue || 0;
     });
   }
-});
+}
+window.addEventListener('turbo:load', calculateProfit);
+window.addEventListener('turbo:render', calculateProfit);
--- app/javascript/tax_price.js
+++ app/javascript/tax_price.js
@@ -1,10 +1,14 @@
-window.addEventListener('turbo:load', () => {
+function calculateTax() {
   const taxInput = document.getElementById('item-price');
   const addTaxDom = document.getElementById("add-tax-price");
-  
+
   if (taxInput && addTaxDom) {
-  taxInput.addEventListener("input", () => {
-    const inputValue = taxInput.value;
-    addTaxDom.innerHTML =  Math.floor(parseFloat(inputValue) * 0.1) || 0;    });
+    taxInput.addEventListener("input", () => {
+      const inputValue = parseFloat(taxInput.value);
+      addTaxDom.innerHTML = Math.floor(inputValue * 0.1) || 0;
+    });
   }
-});
+}
+
+window.addEventListener('turbo:load', calculateTax);
+window.addEventListener('turbo:render', calculateTax);
